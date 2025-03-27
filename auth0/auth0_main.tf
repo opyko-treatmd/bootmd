99a9c050c345ef6c1c99498f260bad993e62fba8
public profile <opyko-treatmd@users.noreply.github.com>
2025-03-27 14:21:06 +0200

    Create auth0_main.tf

---
+++ auth0/auth0_main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_version = ">= 1.5.0"
+  required_providers {
+    auth0 = {
+      source  = "auth0/auth0"
+      version = ">= 1.0.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "CommonRG"
+    storage_account_name = "common4olekpyko202408"
+    container_name       = "github-terraform"
+    key                  = "terraform.tfstate"
+  }
+}
+
+provider "auth0" {
+  debug = true
+}
+
+resource "null_resource" "main" {
+  triggers = {
+    value = "trigger_value"
+  }
+}
